ea7b05b35a7bdf4c8ab291d8babf6de42aff5885
snagjaepark <atimangel@naver.com>
2021-05-25 11:59:20 +0900

    list is working but I wonder is t_list should be double pointer?

--- main.c
+++ main.c
@@ -5,15 +5,22 @@
 
 int	main(int arg_c, char **arg_v)
 {
-	t_list *a;
+	t_list **a;
 
-	a = ft_lstnew(ft_atoi_ptr(arg_v[1]));
-	if (!a->content)
+	a = (t_list **)malloc(sizeof(t_list *));
+	if (!a)
 	{
 		ft_putstr_fd("error\n", 2);
 		return(0);
 	}
-	printf("%d\n", *(int *)a->content);
-	ft_lstdelone(a, free);	
+	ft_lstadd_back(a, ft_lstnew(ft_atoi_ptr(arg_v[1])));
+	if (!(*a)->content)
+	{
+		ft_putstr_fd("error\n", 2);
+		ft_lstclear(a, free);
+		return(0);
+	}
+	printf("%p %p %d\n", a, *a, *(int *)(*a)->content);
+	ft_lstclear(a, free);
 	return (0);
 }
