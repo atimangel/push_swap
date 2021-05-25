b35cfa24f151beddf794a950f4c2b2494ce960d9
snagjaepark <atimangel@naver.com>
2021-05-25 14:18:19 +0900

    main함수에 있던 인자 받아서 리스트 만드는 부분을 함수로 따로 빼냈다. 오류 처리를 다 삽입해서 코드가 길어졌다. 나중에 분리해야겠다.

--- main.c
+++ main.c
@@ -3,6 +3,68 @@
 #include "libft_bonus.h"
 #include "libft.h"
 
+t_list *make_list(int arg_c, char **arg_v)
+{
+	t_list	*a;
+	t_list	*tmp;
+	int	i;
+	int	num;
+	int	*integer;
+
+	i = 1;
+	while (i < arg_c)
+	{
+		if (i == 1)
+		{
+			integer = ft_atoi_ptr(arg_v[i++]);
+			if (!integer)
+			{
+				ft_putstr_fd("error\n", 2);
+				return (0);
+			}
+			a = ft_lstnew(integer);
+			if (!a)
+			{
+				ft_putstr_fd("error\n", 2);
+				return (0);
+			}
+		}
+		else
+		{
+			printf("i = %d\n", i);
+			integer = ft_atoi_ptr(arg_v[i++]);
+			if (!integer)
+			{
+				ft_lstclear(&a, free);
+				ft_putstr_fd("error\n", 2);
+				return (0);
+			}
+			tmp = ft_lstnew(integer);
+			if (!tmp)
+			{
+				free(integer);
+				ft_lstclear(&a, free);
+				ft_putstr_fd("error\n", 2);
+				return (0);
+			}	
+			ft_lstadd_back(&a, tmp);
+			printf("tmp %p end %p num %d\n", tmp, ft_lstlast(a), *(int *)tmp->content);
+			tmp = a;
+			num = *(int *)ft_lstlast(a)->content;
+			while (!tmp)
+			{
+				if (num == *(int *)tmp->content && tmp != ft_lstlast(a))
+				{
+					ft_putstr_fd("error\n", 2);
+					return (0);
+				}
+				tmp = tmp->next;
+			}
+		}
+	}
+	return (a);
+}
+
 int	main(int arg_c, char **arg_v)
 {
 	t_list *a;
@@ -11,37 +73,12 @@ int	main(int arg_c, char **arg_v)
 	int	*num;
 
 	i = 1;
-	while (i < arg_c)
+	a = make_list(arg_c, arg_v);
+	tmp = a;
+	while (tmp)
 	{
-		tmp = a;
-		num = ft_atoi_ptr(arg_v[i++]);
-		if (!num)
-		{
-			ft_putstr_fd("error\n", 2);
-			ft_lstclear(&a, free);
-			return(0);
-		}
-		if (!a)
-			a = ft_lstnew(num);
-		else
-		{
-			while (!tmp)
-			{
-				if (*(int *)tmp->content == *num)
-				{
-					ft_putstr_fd("error\n", 2);
-					ft_lstclear(&a, free);
-					return (0);
-				}
-				if (!tmp->next)
-				{
-					tmp->next = ft_lstnew(num);
-					break;
-				}
-				tmp = tmp->next;
-			}
-		}
-		printf("%d\n", *num);
+		printf("tmp %p a %p num %d\n", tmp, a, *(int *)tmp->content);
+		tmp = tmp->next;
 	}
 	ft_lstclear(&a, free);
 	return (0);
