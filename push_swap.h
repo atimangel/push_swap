9693f20e76802472f8f3aa904ce2ea41709963d7
snagjaepark <atimangel@naver.com>
2021-05-25 15:15:47 +0900

    헤더 만들고 안에 공통적으로 쓰는 헤더를 집어넣음 중복 헤더 방지함

---
+++ push_swap.h
@@ -0,0 +1,22 @@
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+# include "libft.h"
+# include "libft_bonus.h"
+
+/*
+**SWAP
+*/
+void	swap(t_list **list);
+void	sa(t_list **a);
+void	sb(t_list **b);
+void	ss(t_list **a, t_list **b);
+/*
+**PUSH
+*/
+/*
+**ROTATE
+*/
+/*
+**REVERSE ROTATE
+*/
+#endif
