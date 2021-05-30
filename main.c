2c1b2b1a2eaa2e1a9a5214e4a9382d1ba5feee2c
snagjaepark <atimangel@naver.com>
2021-05-30 22:06:49 +0900

    make_list 25 line under

--- main.c
+++ main.c
@@ -1,6 +1,44 @@
 #include "push_swap.h"
 #include <stdio.h>
 
+int	check_same_num(t_list **a, t_list *tmp, int *integer)
+{
+	while (tmp)
+	{
+		if (*integer == *(int *)tmp->content && integer != (int *)tmp->content)
+		{
+			ft_lstclear(a, free);
+			ft_putstr_fd("error\n", 2);
+			return (0);
+		}
+		tmp = tmp->next;
+	}
+	return(1);
+}
+
+int	make_list_fail_integer(int *integer, t_list **a)
+{
+	if (!integer)
+	{
+		ft_lstclear(a, free);
+		ft_putstr_fd("error\n", 2);
+		return (0);
+	}
+	return (1);
+}
+
+int	make_list_fail_tmp(int *integer, t_list *tmp, t_list **a)
+{
+	if (!tmp)
+	{
+		free(integer);
+		ft_lstclear(a, free);
+		ft_putstr_fd("error\n", 2);
+		return (0);
+	}
+	return (1);
+}
+
 t_list *make_list(int arg_c, char **arg_v)
 {
 	t_list	*a;
@@ -14,32 +52,14 @@ t_list *make_list(int arg_c, char **arg_v)
 	while (i < arg_c)
 	{
 		integer = ft_atoi_ptr(arg_v[i++]);
-		if (!integer)
-		{
-			ft_lstclear(&a, free);
-			ft_putstr_fd("error\n", 2);
+		if (!make_list_fail_integer(integer, &a))
 			return (0);
-		}
 		tmp = ft_lstnew(integer);
-		if (!tmp)
-		{
-			free(integer);
-			ft_lstclear(&a, free);
-			ft_putstr_fd("error\n", 2);
+		if (!make_list_fail_tmp(integer, tmp,  &a))
 			return (0);
-		}	
 		ft_lstadd_back(&a, tmp);
-		tmp = a;
-		while (tmp)
-		{
-			if (*integer == *(int *)tmp->content && integer != (int *)tmp->content)
-			{
-				ft_lstclear(&a, free);
-				ft_putstr_fd("error\n", 2);
-				return (0);
-			}
-			tmp = tmp->next;
-		}
+		if (!check_same_num(&a, a, integer))
+			return (0);
 	}
 	return (a);
 }
