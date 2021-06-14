7b438df22557d7f6a55acca4b79f10a1063b2c02
snpark <snpark@student.42seoul.kr>
2021-06-14 10:59:37 +0900

    feature: free all node

--- main.c
+++ main.c
@@ -6,26 +6,31 @@
 /*   By: snpark <snpark@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/31 16:19:49 by snpark            #+#    #+#             */
-/*   Updated: 2021/06/10 13:42:40 by snpark           ###   ########.fr       */
+/*   Updated: 2021/06/14 10:56:10 by snpark           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int		main(int arg_c, char **arg_v)
+static void	set_b_list(t_node_h *b)
+{
+	b->head = 0;
+	b->tail = 0;
+	b->len = 0;
+}
+
+int			main(int arg_c, char **arg_v)
 {
 	t_node_h		a;
-//	t_node_h		b;
-//	t_pivot			a_range;
-//	t_pivot			b_range;
+	t_node_h		b;
+	t_pivot			a_range;
+	t_pivot			b_range;
 
 	if (arg_c == 1)
 		return (0);
 	if (!make_list(arg_c, arg_v, &a))
 		return (0);
-/*	b.head = 0;
-	b.tail = 0;
-	b.len = 0;
+	set_b_list(&b);
 	set_pivot(&a_range, a.len - 1, 0);
 	set_pivot(&b_range, 0, 0);
 	b_range.len = 0;
@@ -36,6 +41,8 @@ int		main(int arg_c, char **arg_v)
 	if (a.len == 3)
 		sort_3_a(&a);
 	if (a.len >= 4)
-		quick_sort(&a, &b, a_range, b_range);*/
+		quick_sort(&a, &b, a_range, b_range);
+	ft_nodeclear(&a);
+	ft_nodeclear(&b);
 	return (0);
 }
